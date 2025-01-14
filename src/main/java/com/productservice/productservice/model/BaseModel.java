7db59776ad7eecfee86aeb3843769b71247b70e7
Ravi Kumar <kumarg2185@gmail.com>
2025-01-14 15:10:37 +0530

    DB Connectivity

--- src/main/java/com/productservice/productservice/model/BaseModel.java
+++ src/main/java/com/productservice/productservice/model/BaseModel.java
@@ -1,6 +1,18 @@
 package com.productservice.productservice.model;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private int id;
 
 }
--- src/main/java/com/productservice/productservice/model/Product.java
+++ src/main/java/com/productservice/productservice/model/Product.java
@@ -1,18 +1,34 @@
 package com.productservice.productservice.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+
+
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
 
+
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+
+    //category isn't a primitive attribute. it's a relation
+    @ManyToOne
     private Category category;
 
+
+/*  1       1
+* product category cardinality
+* m        1
+*
+* */
 
 }
