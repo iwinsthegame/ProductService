cf2b56c826bfee4e303c0327361dab10290c8db9
Ravi Kumar <kumarg2185@gmail.com>
2025-01-14 23:06:39 +0530

    UUID Integration

--- src/main/java/com/productservice/productservice/model/BaseModel.java
+++ src/main/java/com/productservice/productservice/model/BaseModel.java
@@ -1,18 +1,23 @@
 package com.productservice.productservice.model;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
+
+    //UUID INTERNALY GET CONVERTED INTO BINARY ONLY
     @Id
-    private int id;
+    @GeneratedValue(generator = "generator_name")
+    @GenericGenerator(name ="generator_name", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)",nullable = false, updatable = false)
+    private UUID id;
 
 }
